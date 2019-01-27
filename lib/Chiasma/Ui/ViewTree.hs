c57cf7a8483c6a3d468db93a0ba3841f677ea4fd
Torsten Schmits <torstenschmits@gmail.com>
2019-01-27 02:51:29 +0100

    unique subtree modifier

--- lib/Chiasma/Ui/ViewTree.hs
+++ lib/Chiasma/Ui/ViewTree.hs
@@ -1,4 +1,5 @@
 module Chiasma.Ui.ViewTree(
+  toggleLayout,
   togglePane,
   hasOpenPanes,
 ) where
@@ -9,6 +10,7 @@ import Control.Monad.Trans.Except (ExceptT, runExceptT)
 import Control.Monad.Trans.Writer (WriterT, runWriterT, tell)
 import Control.Monad.Trans.Class (lift)
 import Data.Functor.Identity (Identity(..))
+import Data.Traversable (mapAccumL)
 import Data.Monoid (Sum(..))
 import Chiasma.Data.Ident (Ident)
 import Chiasma.Lens.Tree (
@@ -19,18 +21,38 @@ import Chiasma.Lens.Tree (
   LeafIndexTree(..),
   leafDataTraversal,
   )
-import Chiasma.Ui.Data.TreeModError (TreeModError(PaneMissing, AmbiguousPane))
-import Chiasma.Ui.Data.View (Tree, LayoutView, PaneView, ViewTree, View(..), Pane(..))
+import Chiasma.Ui.Data.TreeModError (TreeModError(PaneMissing, AmbiguousPane, LayoutMissing, AmbiguousLayout))
+import Chiasma.Ui.Data.View (Tree(Tree), TreeSub(TreeLeaf), LayoutView, PaneView, ViewTree, View(..), Pane(..))
 import Chiasma.Ui.Pane (paneToggleOpen)
 
-modLogged :: Monad m => (a -> m a) -> a -> WriterT (Sum Int) m a
-modLogged f a = do
+modCounted :: Monad m => (a -> m a) -> a -> WriterT (Sum Int) m a
+modCounted f a = do
   tell (Sum 1)
   lift $ f a
 
-modifyPaneUnique :: Monad m => (PaneView -> m PaneView) -> Ident -> ViewTree -> ExceptT TreeModError m ViewTree
-modifyPaneUnique f ident tree = do
-  let st = (transformM $ mapMOf (ix ident) (modLogged f)) (LeafIndexTree tree)
+treeToggleOpen :: ViewTree -> ViewTree
+treeToggleOpen (Tree l sub) =
+  Tree l (snd $ mapAccumL toggle False sub)
+  where
+    toggle False (TreeLeaf p) = (True, TreeLeaf (paneToggleOpen p))
+    toggle a b = (a, b)
+
+modifyTreeUniqueM :: Monad m => (ViewTree -> m ViewTree) -> Ident -> ViewTree -> ExceptT TreeModError m ViewTree
+modifyTreeUniqueM f ident tree = do
+  let st = (transformM $ mapMOf (ix ident) (modCounted f)) tree
+  (result, Sum count) <- lift $ runWriterT st
+  case count of
+    1 -> return result
+    0 -> throwError $ LayoutMissing ident
+    n -> throwError $ AmbiguousLayout ident n
+
+toggleLayout :: Ident -> ViewTree -> Either TreeModError ViewTree
+toggleLayout ident tree =
+  runIdentity $ runExceptT $ modifyTreeUniqueM (Identity . treeToggleOpen) ident tree
+
+modifyPaneUniqueM :: Monad m => (PaneView -> m PaneView) -> Ident -> ViewTree -> ExceptT TreeModError m ViewTree
+modifyPaneUniqueM f ident tree = do
+  let st = (transformM $ mapMOf (ix ident) (modCounted f)) (LeafIndexTree tree)
   (result, Sum count) <- lift $ runWriterT st
   case count of
     1 -> return $ litTree result
@@ -39,7 +61,7 @@ modifyPaneUnique f ident tree = do
 
 togglePane :: Ident -> ViewTree -> Either TreeModError ViewTree
 togglePane ident tree =
-  runIdentity $ runExceptT $ modifyPaneUnique (Identity . paneToggleOpen) ident tree
+  runIdentity $ runExceptT $ modifyPaneUniqueM (Identity . paneToggleOpen) ident tree
 
 hasOpenPanes :: ViewTree -> Bool
 hasOpenPanes tree =
