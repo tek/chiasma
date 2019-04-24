069d16e6e01bba55960b8b64a5a876a165cf3137
Torsten Schmits <torstenschmits@gmail.com>
2019-04-24 19:51:05 +0200

    `HasPaneId PanePid`

--- lib/Chiasma/Codec/Data/PanePid.hs
+++ lib/Chiasma/Codec/Data/PanePid.hs
@@ -5,7 +5,8 @@ module Chiasma.Codec.Data.PanePid where
 import GHC.Generics (Generic)
 
 import Chiasma.Codec (TmuxCodec)
-import Chiasma.Data.TmuxId (PaneId)
+import Chiasma.Data.TmuxId (HasPaneId, PaneId)
+import qualified Chiasma.Data.TmuxId as HasPaneId (HasPaneId(paneId))
 
 data PanePid =
   PanePid {
@@ -13,3 +14,6 @@ data PanePid =
     panePid :: Int
   }
   deriving (Eq, Show, Generic, TmuxCodec)
+
+instance HasPaneId PanePid where
+  paneId = paneId
