35326ffd5127720058820f34a900947466781bbf
Torsten Schmits <torstenschmits@gmail.com>
2021-07-28 01:07:06 +0200

    remove unused name

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { hix, ... }:
   let
-    overrides = { hackage, only, ... }:
+    overrides = { hackage, only, jailbreak, ... }:
     {
       posix-pty = only "8.6.5" (hackage "0.2.1.1" "0rwb7fj7134lc04dv9vn4j6nq0vadl2qhzlz63pd0an9vqmxw685");
       cornea = hackage "0.4.0.0" "1w9rkf6f861kknkskywb8fczlk7az8m56i3hvmg6a5inpvqf6p7i";
