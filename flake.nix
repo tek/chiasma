d48ee7f7efa7e781e437d94e1d381c56e78764a8
Torsten Schmits <git@tryp.io>
2022-08-21 18:33:04 +0200

    bump ghc

--- flake.nix
+++ flake.nix
@@ -9,14 +9,15 @@
 
   outputs = { hix, prelate, polysemy-conc, ... }:
   let
-    overrides = { hackage, source, unbreak, pkgs, system, buildInputs, ... }: {
+    overrides = { hackage, source, unbreak, pkgs, system, buildInputs, notest, ... }: {
       chiasma-test = buildInputs [pkgs.tmux pkgs.xterm];
-      exon = hackage "0.4.0.0" "098ym81pz8rv88kgf4fmwwh52gz3151j3zvmpmg0a535irajqmq1";
+      exon = hackage "0.5.0.0" "1i5r4ixmxwna3b36hz254jgkm3wbp6iggs71igfhzc9fym1z10hv";
       flatparse = unbreak;
       polysemy = hackage "1.7.1.0" "0qwli1kx3hk68hqsgw65mk81bx0djw1wlk17v8ggym7mf3lailyc";
       polysemy-conc = source.package polysemy-conc "conc";
       polysemy-plugin = hackage "0.4.3.0" "1r7j1ffsd6z2q2fgpg78brl2gb0dg8r5ywfiwdrsjd2fxkinjcg1";
       polysemy-process = source.package polysemy-conc "process";
+      type-errors = notest;
     };
   in hix.lib.flake ({ config, lib, ... }: {
     base = ./.;
