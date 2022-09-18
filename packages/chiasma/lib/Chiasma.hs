7237830709b3e992a16680fbdda55e42ebf39869
Torsten Schmits <git@tryp.io>
2022-09-18 17:55:47 +0200

    create root module

---
+++ packages/chiasma/lib/Chiasma.hs
@@ -0,0 +1,12 @@
+module Chiasma (
+  TmuxApi,
+  Tmux,
+  Codec,
+  NativeCodec,
+  NativeCodecE,
+  NativeCommandCodec,
+  NativeCommandCodecE,
+) where
+
+import Chiasma.Effect.Codec (Codec, NativeCodec, NativeCodecE, NativeCommandCodec, NativeCommandCodecE)
+import Chiasma.Effect.TmuxApi (Tmux, TmuxApi)
