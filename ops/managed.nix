386793901872e9209bf1094729be6bb33745ad9f
hix <hix-bot@github.com>
2026-03-31 13:12:25 +0200

    Release 0.11.0.0
    release-id:chiasma-0.11.0.0
    
    release-id:chiasma-test-0.11.0.0

--- ops/managed.nix
+++ ops/managed.nix
@@ -6,10 +6,10 @@
   solver = {};
   packages = {
     chiasma = {
-      version = "0.10.2.0";
+      version = "0.11.0.0";
     };
     chiasma-test = {
-      version = "0.10.2.0";
+      version = "0.11.0.0";
     };
   };
   resolving = false;
